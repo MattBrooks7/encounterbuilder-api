c3a66ed4373a2c465348478710c961eaf5cbfd1d
Matt Brooks <mattbrooks2010@gmail.com>
2020-12-17 13:29:29 -0600

    routes idk

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
- # resources :monsters
   
-
-  namespace :monsters do 
-    get :index
-    get :show
-    post :create
-    patch :update
-    delete :destroy
+  namespace defaults: ( format: :json) do
+    namespace :monsters do 
+      get :index
+      get :show
+      post :create
+      patch :update
+      delete :destroy
+    end
   end
-
 end
